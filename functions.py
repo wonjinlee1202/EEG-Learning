b0081001d9f3377bb66941d0f2275ec01242ec62
William Shih <63324248+WilliamShih1234@users.noreply.github.com>
2024-03-15 03:03:09 -0700

    updated files

--- functions.py
+++ functions.py
@@ -8,46 +8,44 @@ from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
 '''
-CNN 
+CNN
 '''
 def CNN():
    # Building the CNN model using sequential class
-    hybrid_cnn_lstm_model = Sequential()
+    cnn_model = Sequential()
 
     # Conv. block 1
-    hybrid_cnn_lstm_model.add(Conv2D(filters=25, kernel_size=(5,5), padding='same', activation='elu', input_shape=(400,1,22)))
-    hybrid_cnn_lstm_model.add(MaxPooling2D(pool_size=(3,1), padding='same')) # Read the keras documentation
-    hybrid_cnn_lstm_model.add(BatchNormalization())
-    hybrid_cnn_lstm_model.add(Dropout(0.6))
+    cnn_model.add(Conv2D(filters=25, kernel_size=(5,5), padding='same', activation='elu', input_shape=(400,1,22)))
+    cnn_model.add(MaxPooling2D(pool_size=(3,1), padding='same')) # Read the keras documentation
+    cnn_model.add(BatchNormalization())
+    cnn_model.add(Dropout(0.6))
 
     # Conv. block 2
-    hybrid_cnn_lstm_model.add(Conv2D(filters=50, kernel_size=(5,5), padding='same', activation='elu'))
-    hybrid_cnn_lstm_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
-    hybrid_cnn_lstm_model.add(BatchNormalization())
-    hybrid_cnn_lstm_model.add(Dropout(0.6))
+    cnn_model.add(Conv2D(filters=50, kernel_size=(5,5), padding='same', activation='elu'))
+    cnn_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
+    cnn_model.add(BatchNormalization())
+    cnn_model.add(Dropout(0.6))
 
     # Conv. block 3
-    hybrid_cnn_lstm_model.add(Conv2D(filters=100, kernel_size=(5,5), padding='same', activation='elu'))
-    hybrid_cnn_lstm_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
-    hybrid_cnn_lstm_model.add(BatchNormalization())
-    hybrid_cnn_lstm_model.add(Dropout(0.6))
+    cnn_model.add(Conv2D(filters=100, kernel_size=(5,5), padding='same', activation='elu'))
+    cnn_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
+    cnn_model.add(BatchNormalization())
+    cnn_model.add(Dropout(0.6))
 
     # Conv. block 4
-    hybrid_cnn_lstm_model.add(Conv2D(filters=200, kernel_size=(5,5), padding='same', activation='elu'))
-    hybrid_cnn_lstm_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
-    hybrid_cnn_lstm_model.add(BatchNormalization())
-    hybrid_cnn_lstm_model.add(Dropout(0.6))
+    cnn_model.add(Conv2D(filters=200, kernel_size=(5,5), padding='same', activation='elu'))
+    cnn_model.add(MaxPooling2D(pool_size=(3,1), padding='same'))
+    cnn_model.add(BatchNormalization())
+    cnn_model.add(Dropout(0.6))
 
-    # FC+LSTM layers
-    hybrid_cnn_lstm_model.add(Flatten()) # Adding a flattening operation to the output of CNN block
-    hybrid_cnn_lstm_model.add(Dense((40))) # FC layer with 100 units
-    hybrid_cnn_lstm_model.add(Reshape((40,1))) # Reshape my output of FC layer so that it's compatible
-    hybrid_cnn_lstm_model.add(LSTM(10, dropout=0.4, recurrent_dropout=0.1, input_shape=(40,1), return_sequences=False))
-
+    cnn_model.add(Flatten()) # Adding a flattening operation to the output of CNN block
+    cnn_model.add(Dense((40))) # FC layer with 100 units
+    cnn_model.add(Reshape((40,1))) # Reshape my output of FC layer so that it's compatible
+    cnn_model.add(CNN(10, dropout=0.4, recurrent_dropout=0.1, input_shape=(40,1), return_sequences=False))
 
     # Output layer with Softmax activation 
-    hybrid_cnn_lstm_model.add(Dense(4, activation='softmax')) # Output FC layer with softmax activation
-    return hybrid_cnn_lstm_model
+    cnn_model.add(Dense(4, activation='softmax')) # Output FC layer with softmax activation
+    return cnn_model
 
 '''
 CNN + LSTM
